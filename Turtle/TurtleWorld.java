8ebd0c7d20aa9c4d28f2f8446b134e96a8ab28ee
hhaldimann <hunterhaldimann@sagetreellc.com>
2014-09-02 10:51:09 -0500

    Made turtle write 'Hi!'

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,40 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
+        turtle.setPenWidth(5);
         turtle.penDown();
         turtle.forward(50);
+        turtle.setPenColor(Color.BLUE);
+        turtle.backward(25);
+        turtle.turnRight();
+        turtle.penDown();
+        turtle.forward(25);
+        turtle.turnLeft();
+        turtle.forward(25);
+        turtle.backward(50);
+        turtle.turnRight();
+        turtle.penUp();
+        turtle.forward(10);
+        turtle.turnLeft();
+        turtle.penDown();
+        turtle.forward(50);
+        turtle.backward(50);
+        turtle.penUp();
+        turtle.turnRight();
+        turtle.forward(10);
+        turtle.penDown();
+        turtle.turnLeft();
+        turtle.forward(3);
+        turtle.penUp();
+        turtle.forward(5);
+        turtle.penDown();
+        turtle.forward(42);
+        turtle.penUp();
+        turtle.turnLeft();
+        turtle.forward(140);
+      
+      
+
     }
 }
