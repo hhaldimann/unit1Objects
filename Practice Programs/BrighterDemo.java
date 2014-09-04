f931c1b367d5888056e5bf93f77b7869624edb36
hhaldimann <hunterhaldimann@sagetreellc.com>
2014-09-04 11:24:39 -0500

    color demo

---
+++ Practice Programs/BrighterDemo.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class BrighterDemo
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(200, 200);
+        Color myColor = Color.PINK;
+        frame.getContentPane().setBackground(myColor);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+        
